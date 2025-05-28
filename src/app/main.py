97ac02c1d59818a08a4d6fd0d70b2a67e57e1c3b
Guimaaz <gustavoguimaraesr@gmail.com>
2025-05-28 11:39:32 -0300

    fix : tests is now correct , finish get all UsersData

--- src/app/main.py
+++ src/app/main.py
@@ -16,5 +16,14 @@ app = FastAPI()
 
 repo = Environments.get_UserData_repo()()
 
+@app.get("/UsersData/get_all_usersData")
+def get_all_usersData() :
+    UsersData = repo.get_all_usersData()
+    UsersData_list = list()
+    for UserData in UsersData:
+        UsersData_list.append(UserData.to_dict())
 
+    return {
+        "UsersData" : UsersData_list
+    }
 handler = Mangum(app, lifespan="off")
--- src/app/repo/userData_repository_mock.py
+++ src/app/repo/userData_repository_mock.py
@@ -8,9 +8,9 @@ class UserDataRepositoryMock(IUserDataRepository) :
 
     def __init__(self) :
         self.UsersData = [
-        UserData("Gustavo", "1234", "12345-6", 4000.0),
-        UserData("Vitor", "4321", "65432-1", 0.0), # sobra nada pro soller 
-        UserData("Josefina", "2134", "01234-5", 12000.0)
+        UserData('Gustavo', '1234', '12345-6', 4000.0),
+        UserData('Vitor', '4321', '65432-1', 0.0), # sobra nada pro soller 
+        UserData('Josefina', '2134', '01234-5', 12000.0)
         ] 
     def get_all_usersData(self) -> List[UserData] :
         return  self.UsersData
