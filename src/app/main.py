3579cb909d9985148ff1aa06617524ca9082aaf8
Guimaaz <gustavoguimaraesr@gmail.com>
2025-05-28 22:58:54 -0300

    fix : I FORGOT THE RIGHT ENVIORONMENTS ( im stupid )

--- src/app/main.py
+++ src/app/main.py
@@ -3,12 +3,13 @@ from mangum import Mangum
 
 from .environments import Environments
 
-
+from .repo.item_repository_mock import ItemRepositoryMock
 
 from .errors.entity_errors import ParamNotValidated
 
+from .enums.item_type_enum import ItemTypeEnum
 
-
+from .entities.item import Item
 from .entities.user import UserData
 
 
@@ -26,4 +27,22 @@ def get_all_usersData() :
     return {
         "UsersData" : UsersData_list
     }
+handler = Mangum(app, lifespan="off")
+
+
+app = FastAPI()
+
+repo = Environments.get_UserData_repo()()
+
+@app.get("/items/get_all_items")
+def get_all_items():
+    items = repo.get_all_items()
+    return {
+        "items": [item.to_dict() for item in items]
+    }
+
+
+    
+
+
 handler = Mangum(app, lifespan="off")
