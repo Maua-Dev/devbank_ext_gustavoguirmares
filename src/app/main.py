1602eb64276417e417fe5f1bcd1de0f977684907
Guimaaz <gustavoguimaraesr@gmail.com>
2025-05-28 23:00:06 -0300

    esquece, fui no git e fui no initial commit ver se eu era burro e apaguei algo

--- src/app/main.py
+++ src/app/main.py
@@ -2,47 +2,17 @@ from fastapi import FastAPI, HTTPException
 from mangum import Mangum
 
 from .environments import Environments
-
-from .repo.item_repository_mock import ItemRepositoryMock
-
-from .errors.entity_errors import ParamNotValidated
-
-from .enums.item_type_enum import ItemTypeEnum
-
-from .entities.item import Item
 from .entities.user import UserData
 
 
 app = FastAPI()
 
-repo = Environments.get_UserData_repo()()
+
+repo_user = Environments.get_UserData_repo()()
 
 @app.get("/UsersData/get_all_usersData")
-def get_all_usersData() :
-    UsersData = repo.get_all_usersData()
-    UsersData_list = list()
-    for UserData in UsersData:
-        UsersData_list.append(UserData.to_dict())
-
+def get_all_usersData():
+    users_data = repo_user.get_all_usersData()
     return {
-        "UsersData" : UsersData_list
+        "UsersData": [user.to_dict() for user in users_data]
     }
-handler = Mangum(app, lifespan="off")
-
-
-app = FastAPI()
-
-repo = Environments.get_UserData_repo()()
-
-@app.get("/items/get_all_items")
-def get_all_items():
-    items = repo.get_all_items()
-    return {
-        "items": [item.to_dict() for item in items]
-    }
-
-
-    
-
-
-handler = Mangum(app, lifespan="off")
