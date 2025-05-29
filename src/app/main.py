5f50569a2f45a00b44d850389fd4f36bd4bffe8d
Guimaaz <gustavoguimaraesr@gmail.com>
2025-05-29 13:41:01 -0300

    new route for get user by name

--- src/app/main.py
+++ src/app/main.py
@@ -2,19 +2,27 @@ from fastapi import FastAPI, HTTPException
 from mangum import Mangum
 
 from .environments import Environments
-from .entities.user import UserData
-
 
 app = FastAPI()
 
-
-repo_user = Environments.get_UserData_repo()()
+repo = Environments.get_UserData_repo()()
 
 @app.get("/UsersData/get_all_usersData")
 def get_all_usersData():
-    users_data = repo_user.get_all_usersData()
+    UsersData = repo.get_all_usersData()
     return {
-        "UsersData": [user.to_dict() for user in users_data]
+        "UsersData": [user.to_dict() for user in UsersData]
+    }
+
+@app.get("/UsersData/get_userData_by_name")
+def get_userData_by_name(name: str):
+    user = repo.get_userData_by_name(name)
+    if user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+
+    return {
+        "name": name,
+        "UserData": user.to_dict()
     }
 
 
