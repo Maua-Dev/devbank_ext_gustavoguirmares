2e263d4672088513e342a4ea0189630e3179363c
Guimaaz <gustavoguimaraesr@gmail.com>
2025-05-29 13:45:38 -0300

    fix return model

--- src/app/main.py
+++ src/app/main.py
@@ -5,24 +5,23 @@ from .environments import Environments
 
 app = FastAPI()
 
-repo = Environments.get_UserData_repo()()
+repo_user = Environments.get_UserData_repo()()
 
 @app.get("/UsersData/get_all_usersData")
 def get_all_usersData():
-    UsersData = repo.get_all_usersData()
+    UsersData = repo_user.get_all_usersData()
     return {
         "UsersData": [user.to_dict() for user in UsersData]
     }
 
 @app.get("/UsersData/get_userData_by_name")
 def get_userData_by_name(name: str):
-    user = repo.get_userData_by_name(name)
-    if user is None:
+    userData = repo_user.get_userData_by_name(name)
+    if userData is None:
         raise HTTPException(status_code=404, detail="User not found")
 
     return {
-        "name": name,
-        "UserData": user.to_dict()
+         "UsersData": [user.to_dict() for user in userData]
     }
 
 
